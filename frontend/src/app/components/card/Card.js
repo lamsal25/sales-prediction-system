94c7a0ab1a11ef5a1d107fc3f927d1f666acc205
SumanGautam1 <gautamsuman822@gmail.com>
2024-08-18 21:51:24 +0545

    Carousel added, feature cards modified, small bullet added in form

--- frontend/src/app/components/card/Card.js
+++ frontend/src/app/components/card/Card.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-export default function Card({title1, title2, imagesrc}) {
+export default function Card({ title1, title2, imagesrc }) {
   return (
-    
-        <div className='m-auto rounded-lg h-96 w-1/4  border-2 shadow-blue-400 shadow-lg mt-10 bg-blue-950 text-white'>
-            <img src={imagesrc}  className="w-full h-56 rounded-lg border-2 border-black" />
-            <h3 className='text-center mt-14  text-2xl'>
-              {title1}<br/> {title2}
-            </h3>
-        </div>
-   
+
+    <div className='border-4 border-[#1C2C4C] hover:border-blue-400 bg-blue-400 cursor-pointer group m-auto rounded-xl h-auto w-1/4 shadow-blue-400 shadow-xl mt-10 bg-blue-950 hover:bg-[#1C2C4C] hover:text-white '>
+      <img src={imagesrc} className="w-full rounded-lg p-7 hover:invert " />
+      
+      <h3 style={{ textShadow: '5px 5px 10px #1C2C4C' }} className='text-center p-4 text-2xl font-bold hidden group-hover:block bg-white text-black'>
+        {title1}<br /> {title2}
+      </h3>
+    </div>
+
+
   )
 }
--- frontend/src/app/components/hero/Hero.js
+++ frontend/src/app/components/hero/Hero.js
@@ -1,18 +1,67 @@
+"use client";
 import React from 'react';
+import { Carousel } from "@material-tailwind/react";
+
+import Image from 'next/image';
 
 export default function Hero() {
     return (
-        <div className='relative'>
-            <div className='relative w-full h-[90vh]'>
-                <img src="images/heroimg.jpg" alt="Hero Image" className='w-full h-full object-cover' />
-                <div className='absolute inset-0 bg-[#1C2C4C] opacity-60 mix-blend-multiply'></div>
-            </div>
-            <div className='absolute inset-36 flex flex-col text-white'>
-                <h2 className='text-6xl leading-snug'>Welcome!</h2>
-                <p className='text-2xl leading-snug mt-10'>
-                    Your Trusted Tool for <br /> Sales forecasting
-                </p>
-            </div>
+        <div>
+            <Carousel className="rounded-xl">
+                <div className="relative h-[90vh] w-full">
+                    <img
+                        src="images/img1.jpg"
+                        alt="image 1"
+                        className="h-full w-full object-cover"
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-50"></div> {/* Overlay */}
+
+                    <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+                        <h1 className='text-2xl text-white [font-variant:small-caps] mt-[20%]'>
+                            "Data science is the ability to take data, understand it, process it, extract value from it, visualize it, communicate it."
+                        </h1>
+                        <p className='text-white mt-4'>
+                            - DJ Patil (Former U.S. Chief Data Scientist, LinkedIn)
+                        </p>
+                    </div>
+                </div>
+
+
+                <div className="relative h-[90vh] w-full">
+                    <img
+                        src="images/img2.webp"
+                        alt="image 2"
+                        className="h-full w-full object-cover"
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-50"></div> {/* Overlay */}
+
+                    <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+                        <h1 className='text-2xl text-white [font-variant:small-caps] mt-[20%]'>
+                            "In deep learning, we don't know how our networks work. We just have to trust them."                        </h1>
+                        <p className='text-white mt-4'>
+                            -Geoffrey Hinton (Pioneer in Neural Networks, Google)
+
+                        </p>
+                    </div>
+                </div>
+
+                <div className="relative h-[90vh] w-full">
+                    <img
+                        src="images/img3.jpg"
+                        alt="image 3"
+                        className="h-full w-full object-cover"
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-50"></div> {/* Overlay */}
+
+                    <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+                        <h1 className='text-2xl text-white [font-variant:small-caps] mt-[20%]'>
+                            "Data science is about 80% data preparation, 20% complaining about the need to prepare data."                        </h1>
+                        <p className='text-white mt-4'>
+                            -Hadley Wickham (Chief Scientist at RStudio, Creator of ggplot2)
+                        </p>
+                    </div>
+                </div>
+            </Carousel>
         </div>
     );
 }
