a6b4d31243297ddfe1fa860e7fdb033523b76838
SumanGautam1 <gautamsuman822@gmail.com>
2024-09-12 23:18:12 +0545

    added register and login functions

--- backend/sales/urls.py
+++ backend/sales/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('register/', views.register, name='register'),
+path('login/', views.log_in, name='login'),
 ]
--- backend/sales/views.py
+++ backend/sales/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
-from .serializers import UserSerializer
 from rest_framework.response import Response
-from django.http import JsonResponse
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
 @api_view(['POST'])
 def register(request):
-    if  request.method== 'POST':
-        print(request.data)
-        registerserializer=UserSerializer(data=request.data)
-        if registerserializer.is_valid():
-            registerserializer.save()
-            return JsonResponse({'status':'successful','message':'Registration Succesful'}) 
-        return JsonResponse({'status':'error','message':'Registration Failure'})
+    data = request.data
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    password1 = data.get('password1')
+    if(password==password1):
+        if User.objects.filter(username=username).exists(): # Pailei username registered xa db ma vane error return garxa
+            return Response({'message': 'Username already exists!'})
+        elif User.objects.filter(email=email).exists():
+            return Response({'message': 'Email already exists!'})
+        else:
+            User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            return Response({'message': 'Registered successfully'})
+    else:
+        return Response({'message': 'Passwords doesn\'t match!'})
+
+
+@api_view(['POST'])
+def log_in(request):
+    data = request.data
+    username = data.get('username')
+    password = data.get('password')
+    user=authenticate(username=username,password=password)
+    if not User.objects.filter(username=username).exists():
+        return Response({'message': 'User doesn\'t exists!'})
+    if user is not None:
+        login(request,user)
+        return Response({'message': 'Login successful'})
+    else:
+        return Response({'message': 'Try Again!'})
